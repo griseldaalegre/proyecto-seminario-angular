1627795f1fdec6d69d5ba663b848c2699a9a0335
griseldaalegre <alegregriselda5@gmail.com>
2024-10-07 14:47:30 -0300

    Se arreglo desplegable de navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        if (navbarToggler) {
+          const bsCollapse = new (window as any).bootstrap.Collapse(document.querySelector('#navbarNav'), {
+            toggle: false
+          });
+          bsCollapse.hide();
+        }
+      }
+    });
+  }
 }
